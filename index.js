75a6b2bf2dd07ef7f6f8fdc42ff544ea9b51c949
brojor <bronislav.jordan@gmail.com>
2023-03-04 16:44:46 +0100

    faat: use spaces for indentation

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ import addRecordToNotion from './notion.js'
 $.verbose = false
 
 async function getUrl() {
-	const url = await $`osascript -e 'tell application "Google Chrome" to return URL of active tab of front window'`
+  const url = await $`osascript -e 'tell application "Google Chrome" to return URL of active tab of front window'`
   return url.toString().trim()
 }
 
 function echoMessage(url) {
-	const actualUrl = chalk.underline(url)
-	const pageName = chalk.green('TODAY I DISCOVERED')
-	const appName = chalk.green('Notion')
-	echo `Na stránku ${pageName} v aplikaci ${appName} bude uložen odkaz: ${actualUrl}}\n`
+  const actualUrl = chalk.underline(url)
+  const pageName = chalk.green('TODAY I DISCOVERED')
+  const appName = chalk.green('Notion')
+  echo `Na stránku ${pageName} v aplikaci ${appName} bude uložen odkaz: ${actualUrl}}\n`
 }
 
 async function main() {
-	const url = await getUrl()
-	echoMessage(url)
-	const description = await question(chalk.cyan('Zadejte prosím krátký popis: '))
-	await addRecordToNotion(url, description)
-	echo `\n✅ Odkaz byl uložen.`
+  const url = await getUrl()
+  echoMessage(url)
+  const description = await question(chalk.cyan('Zadejte prosím krátký popis: '))
+  await addRecordToNotion(url, description)
+  echo `\n✅ Odkaz byl uložen.`
 }
 
 main()
