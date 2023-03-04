c49ae9f0f02456db9e897a437bccbfd562d46f01
brojor <bronislav.jordan@gmail.com>
2023-03-04 16:18:26 +0100

    fix: bug in block objects helper

--- object-helpers.js
+++ object-helpers.js
@@ -1,26 +1,26 @@
 class Block {
-		constructor() {
-				this.object = "block";
-		}
+	constructor() {
+			this.object = "block";
+	}
 }
 
 export class Heading extends Block {
-		constructor(title, level = 3, isToggleable = true) {
-				super();
-				this.type = "heading_" + level;
-				this.heading = {
-						rich_text: [{ type: "text", text: { content: title } }],
-						is_toggleable: isToggleable,
-				};
-		}
+	constructor(title, level = 3, isToggleable = true) {
+			super();
+			this.type = "heading_" + level;
+			this[`heading_${level}`] = {
+					rich_text: [{ type: "text", text: { content: title } }],
+					is_toggleable: isToggleable,
+			};
+	}
 }
 
 export class ParagraphWithLink extends Block {
-		constructor(text, url) {
-				super();
-				this.type = "paragraph";
-				this.paragraph = {
-						rich_text: [{ type: "text", text: { content: text, link: { url } } }],
-				};
-		}
+	constructor(text, url) {
+			super();
+			this.type = "paragraph";
+			this.paragraph = {
+					rich_text: [{ type: "text", text: { content: text, link: { url } } }],
+			};
+	}
 }
